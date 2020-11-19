c13247964463021f7a80f54b88dfdb3d98e6a1bc
Peter Vayda <petertvayda@gmail.com>
2020-11-19 13:55:11 -0800

    connected client to api to db

--- sr_db/init.sql
+++ sr_db/init.sql
@@ -1,4 +1,4 @@
-CREATE TABLE service_requests (id serial PRIMARY KEY, sr_name text, contractor text, task text);
-INSERT INTO service_requests (sr_name, contractor, task) VALUES 
+CREATE TABLE service_requests (id serial PRIMARY KEY, sr_name text, sr_contractor text, sr_task text);
+INSERT INTO service_requests (sr_name, sr_contractor, sr_task) VALUES 
     ('water heater', 'Good guy LLC', 'inspect'),
     ('paint house', 'quality painters inc', 'paint house');
